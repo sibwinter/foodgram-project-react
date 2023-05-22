d57fc829c23e21790a53ba46eb7adda0bcf99e36
Denis Seredin <sibwinter42@gmail.com>
2023-05-22 11:16:39 +0300

    add ingredient filter

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -29,6 +29,10 @@ class RecipeFilter(rest_framework.FilterSet):
         queryset=Tag.objects.all()
     )
 
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags')
+        
     def is_favorited_method(self, queryset, name, value):
         if self.request.user.is_anonymous:
             return Recipe.objects.none()
@@ -54,6 +58,3 @@ class RecipeFilter(rest_framework.FilterSet):
 
         return queryset.filter(id__in=recipe)
 
-    class Meta:
-        model = Recipe
-        fields = ('author', 'tags')
