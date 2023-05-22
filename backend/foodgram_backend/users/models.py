c6f52615f6653f65cded1a5305367135e0f2cb03
Denis Seredin <sibwinter42@gmail.com>
2023-05-22 09:48:38 +0300

    settings constant to model

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 
+
 class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [
