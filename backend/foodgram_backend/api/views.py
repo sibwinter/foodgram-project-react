5040e381ce31819e723558fd3de1633900d45f3d
Denis Seredin <sibwinter42@gmail.com>
2023-05-23 20:53:49 +0300

    filter fix

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -97,7 +97,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     permission_classes = (IsAuthorOrAdminPermission,)
     filter_backends = (DjangoFilterBackend,)
-    filterset_class = (RecipeFilter, ,)
+    filterset_class = RecipeFilter
     pagination_class = CustomPageNumberPagination
 
     def get_serializer_class(self):
