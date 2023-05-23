1bda807444717dd1d205fa9011d50fb33b0b71fb
Denis Seredin <sibwinter42@gmail.com>
2023-05-23 20:58:54 +0300

    filter fix

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -90,7 +90,8 @@ class IngredientViewSet(viewsets.ModelViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filterset_class = IngredientFilter
+    filterset_class = IngredientFilter    
+    filter_backends = (DjangoFilterBackend,)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
