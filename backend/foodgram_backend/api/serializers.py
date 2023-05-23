32b97ee2a4f37baedd17d2b6e6825447e063f49a
Denis Seredin <sibwinter42@gmail.com>
2023-05-23 22:37:39 +0300

    subscribe fix

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -2,6 +2,7 @@ from django.core import exceptions
 from django.core.validators import MinValueValidator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from drf_extra_fields.fields import Base64ImageField
 
 from recipes.models import Recipe, Ingredient, Favourite, ShoppingCart
@@ -62,6 +63,27 @@ class SubscriptionSerializer(CurrentUserSerializer):
         fields = ('id', 'username', 'first_name', 'last_name', 'email',
                   'is_subscribed', 'recipes', 'recipes_count')
 
+
+class SubscriptionCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания подписки на автора."""
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'author'),
+                message='Вы уже подписаны на этого пользователя.'
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['author']:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя.')
+        return data
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
