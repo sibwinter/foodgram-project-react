68ac204b43bc504d979391bedc8a08f85a25a098
Denis Seredin <sibwinter42@gmail.com>
2023-05-26 23:38:13 +0300

    admin fix

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Follow, User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = (
         'username',
         'id',
@@ -12,9 +13,7 @@ class UserAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
     )
-    list_filter = ('email', 'username')
-    search_fields = ('username',)
-    empty_value_display = '-пусто-'
+    list_filter = ('email', 'first_name')
 
 
 @admin.register(Follow)
